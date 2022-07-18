bbb0ee80d7bdf2c1ac6dec16d3b467ad540bbc96
FauziaShafi <fauziashafi12@gmail.com>
2022-07-18 15:12:41 -0700

    Added models

---
+++ Server/routes/auth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+// Create a user "/api/auth/createuser"
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => console.log(err));
+  }
+);
+module.exports = router;
