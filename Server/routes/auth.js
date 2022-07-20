49b37785aa5891b849ddf3181dd66acad7398f15
FauziaShafi <fauziashafi12@gmail.com>
2022-07-20 15:44:58 -0700

    Added route for signup

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -13,19 +13,31 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => console.log(err));
+    // Check if the user already exists
+    try {
+      const user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+      // Create a new user
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json(user);
+      //Catch errors
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error Occured");
+    }
   }
 );
 module.exports = router;
